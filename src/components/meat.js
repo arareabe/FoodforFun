00190cb27f90e7030476022202182dc2d31e0b82
Abe Kim <abe1436@MSI.localdomain>
2022-10-30 21:02:19 -0700

    Added home page with components meats and popular

---
+++ src/components/meat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Meats() {
+  return (
+    <div>Tasty Meats</div>
+  )
+};
+
+export default Meats;
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import Popular from '../components/popular';
+import Meats from '../components/meat';
+
+function Home() {
+  return (
+    <>
+      <Popular />
+      <Meats />
+    </>
+  )
+}
+
+export default Home;
